8dead8e5e6a4744fcf74f145d59523a9f3493c3a
Wijkarn <89905143+Wijkarn@users.noreply.github.com>
2023-05-06 16:24:53 +0200

    Delete src directory

--- src/js/main.jsx
+++
@@ -1,6 +0,0 @@
-// import { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from '../components/App';
-
-const body = createRoot(document.querySelector("body"));
-body.render(<App/>);
